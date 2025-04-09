be940e52bdee5b794d5f97e719a1d36e569ef31e
Mati Joseph <crucialniccur@gmail.com>
2025-04-09 14:59:23 +0300

    create the Links component , define boilerplate and add an export default

---
+++ src/components/Links.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Links(props) {
+  return (
+    <>
+      <h3>Links</h3>
+      <a href="">Github content here</a>
+      <a href="">LinkedIn content here</a>
+    </>
+  );
+}
+
+export default Links;
